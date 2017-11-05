e098f6bdf4509212d9ea2be996cfd615418487e7
gabriel barrett <gbarrett2@gmail.com>
2017-11-04 21:12:04 -0600

    created locationVM

--- Forks1/Models/Location.cs
+++ Forks1/Models/Location.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Forks1.Models
 {
     public class Location
     {
+        [Key]
+        public int LocationId { get; set; }
+
+        public string LocationName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
     }
 }
--- Forks1/Models/LocationViewModel.cs
+++ Forks1/Models/LocationViewModel.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Forks1.Models
 {
     public class LocationViewModel
     {
+        [Key]
+        public int LocationId { get; set; }
+
+        [Display(Name = "Location")]
+        public string LocationName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        public string ZipCode { get; set; }
     }
 }
